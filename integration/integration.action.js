5bb29ad72b3a4a5a7d030b6a7e22b25d6c6c9f40
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-04-28 11:10:55 -0500

    Update integration.action.js

--- integration/integration.action.js
+++ integration/integration.action.js
@@ -1,3 +1,10 @@
 exports.onExecutePreUserRegistration = async (event, api) => {
-  console.log(event, api);
+  const { APPROVED_DOMAINS, ERROR, USER_ERROR_MESSAGE } = event.configuration;
+
+  const approvedDomains = APPROVED_DOMAINS.split(',');
+  const userDomain = event?.user?.email?.split('@')[1];
+
+  if (userDomain && !approvedDomains.includes(userDomain)) {
+    api.access.deny(ERROR, USER_ERROR_MESSAGE);
+  }
 };
